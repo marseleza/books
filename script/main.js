7bb7dd91c366e3bcd22dd2f12fe76e24e731e820
dekabu <dyonya0.0@gmail.com>
2024-08-08 02:22:44 +0400

    c

--- script/main.js
+++ script/main.js
@@ -1,11 +1,13 @@
+//тут будет список книг
+var books
+
+//запрос на получение json
 var request = new XMLHttpRequest()
 
 request.open("GET", "list.json")
-
 request.responseType = "json"
-
 request.send()
 
 request.onload = function() {
-	var books = request.response
+	window[books] = request.response
 }
