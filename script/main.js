fb8e450de74d0eeb7245f17a2963b04043cfaa2f
dekabu <dyonya0.0@gmail.com>
2024-08-08 03:10:44 +0400

    c

--- script/main.js
+++ script/main.js
@@ -1,11 +1,11 @@
 //запрос на получение json
 var request = new XMLHttpRequest()
 
-request.open("GET", "list.json")
+request.open("GET", "https://dekabu.github.io/books/list.json")
 request.responseType = "json"
 request.send()
 
 request.onload = function() {
-	var books = request.response
-	console.log(books)
+	var list = request.response
+	console.log(list)
 }
